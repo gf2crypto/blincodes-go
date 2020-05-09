c1fe86b78de405c1dcd9d724350625ed51c049c9
Ivan Chizhov <ivchizhov@gmail.com>
2020-05-09 22:42:36 +0300

    Add tests for NOT function of Vector object

---
+++ vector/vector_not_test.go
@@ -0,0 +1,118 @@
+package vector
+
+import (
+    "testing"
+)
+
+//TestNotNil tests function NOT for nil vector
+func TestNotNil(t *testing.T) {
+    v := New(nil)
+    v.Not()
+    if v.Len() != 0 {
+        t.Errorf("vector testing: NOT nil is incorrect, NOT nil != nil, but %v",
+            v)
+    }
+}
+
+//TestNotEmpty tests function Not of empty vector
+func TestNotEmpty(t *testing.T) {
+    v := New([]uint8{})
+    v.Not()
+    if v.Len() != 0 {
+        t.Errorf("vector testing: Not empty is incorrect, Not empty != nil, but %v",
+            v)
+    }
+}
+
+//TestNotLess64 tests function Not for vectors of length less than 64
+func TestNotLess64(t *testing.T) {
+    v := New([]uint8{
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+    })
+    u := New([]uint8{
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+    })
+    res := New([]uint8{
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+    })
+    v.Not()
+    if v.Len() != 32 {
+        t.Errorf("vector testing: Not is incorrect, length of result != 32 (%d != 32)",
+            v.Len())
+    }
+    if v.body[0] != res.body[0] {
+        t.Errorf("vector testing: Not is incorrect, Not %v = %v != %v",
+            u, v, res)
+    }
+}
+
+//TestNot64 tests function Not for vectors of length 64
+func TestNot64(t *testing.T) {
+    v := New([]uint8{
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1,
+        0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1,
+    })
+    u := New([]uint8{
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1,
+        0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1,
+    })
+    res := New([]uint8{
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+        1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0,
+    })
+    v.Not()
+    if v.Len() != 64 {
+        t.Errorf("vector testing: Not is incorrect, length of result != 32 (%d != 32)",
+            v.Len())
+    }
+    if v.body[0] != res.body[0] {
+        t.Errorf("vector testing: Not is incorrect, Not %v = %v != %v",
+            u, v, res)
+    }
+}
+
+//TestNotMore64 tests function Not for vectors of length more than 64
+func TestNotMore64(t *testing.T) {
+    v := New([]uint8{
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1,
+        0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1,
+        0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
+        0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    })
+    u := New([]uint8{
+        1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+        0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1,
+        0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1,
+        0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1,
+        0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    })
+    res := New([]uint8{
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+        1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0,
+        1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0,
+        1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0,
+        1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    })
+    v.Not()
+    if v.Len() != 93 {
+        t.Errorf("vector testing: Not is incorrect, length of result != 32 (%d != 32)",
+            v.Len())
+    }
+    if v.body[0] != res.body[0] && res.body[0] != res.body[1] {
+        t.Errorf("vector testing: Not is incorrect, Not %v = %v != %v",
+            u, v, res)
+    }
+}
