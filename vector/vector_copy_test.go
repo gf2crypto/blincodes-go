48cf4fff8adaf89c97b20172f7d438648af2019c
Ivan Chizhov <ivchizhov@gmail.com>
2020-05-11 23:56:14 +0300

    Rewrite tests using function Equal

--- vector/vector_copy_test.go
+++ vector/vector_copy_test.go
@@ -8,13 +8,9 @@ import (
 func TestCopyNil(t *testing.T) {
     v := New(nil)
     w := v.Copy()
-    if len(w.body) != 0 {
-        t.Errorf("vector testing: Copy() is incorrect, len(Copy().body) != 0 (%d != 0)",
-            len(w.body))
-    }
-    if w.lenLast != 0 {
-        t.Errorf("vector testing: Copy() is incorrect, Copy().lenLast != 0 (%d != 0)",
-            w.lenLast)
+    if !w.Equal(v) {
+        t.Errorf("vector testing: Copy() is incorrect, %v != %v",
+            w, v)
     }
 }
 
@@ -22,13 +18,9 @@ func TestCopyNil(t *testing.T) {
 func TestCopyEmpty(t *testing.T) {
     v := New([]uint8{})
     w := v.Copy()
-    if len(w.body) != 0 {
-        t.Errorf("vector testing: Copy() is incorrect, len(Copy().body) != 0 (%d != 0)",
-            len(w.body))
-    }
-    if w.lenLast != 0 {
-        t.Errorf("vector testing: Copy() is incorrect, Copy().lenLast != 0 (%d != 0)",
-            w.lenLast)
+    if !w.Equal(v) {
+        t.Errorf("vector testing: Copy() is incorrect, %v != %v",
+            w, v)
     }
 }
 
@@ -36,19 +28,9 @@ func TestCopyEmpty(t *testing.T) {
 func TestCopyLen1(t *testing.T) {
     v := New([]uint8{0})
     w := v.Copy()
-    if len(w.body) != len(v.body) {
-        t.Errorf("vector testing: Copy() is incorrect, len(v.Copy()) != v (%d != %d)",
-            len(w.body), len(v.body))
-    }
-    for i, b := range w.body {
-        if b != v.body[i] {
-            t.Errorf("vector testing: Copy() is incorrect, v.Copy().body != v.body (%v != %v)",
-                w.body, v.body)
-        }
-    }
-    if w.lenLast != v.lenLast {
-        t.Errorf("vector testing: Copy() is incorrect, v.Copy().lenLast != v.lenLast (%v != %v)",
-            w.lenLast, v.lenLast)
+    if !w.Equal(v) {
+        t.Errorf("vector testing: Copy() is incorrect, %v != %v",
+            w, v)
     }
 }
 
@@ -59,19 +41,9 @@ func TestCopyLenLess64(t *testing.T) {
         0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
     })
     w := v.Copy()
-    if len(w.body) != len(v.body) {
-        t.Errorf("vector testing: Copy() is incorrect, len(v1.Copy()) != v1 (%d != %d)",
-            len(w.body), len(v.body))
-    }
-    for i, b := range w.body {
-        if b != v.body[i] {
-            t.Errorf("vector testing: Copy() is incorrect, v1.Copy().body != v1.body (%v != %v)",
-                w.body, v.body)
-        }
-    }
-    if w.lenLast != v.lenLast {
-        t.Errorf("vector testing: Copy() is incorrect, v1.Copy().lenLast != v1.lenLast (%v != %v)",
-            w.lenLast, v.lenLast)
+    if !w.Equal(v) {
+        t.Errorf("vector testing: Copy() is incorrect, %v != %v",
+            w, v)
     }
 }
 
@@ -84,19 +56,9 @@ func TestCopyLen64(t *testing.T) {
         0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1,
     })
     w := v.Copy()
-    if len(w.body) != len(v.body) {
-        t.Errorf("vector testing: Copy() is incorrect, len(v2.Copy()) != v2 (%d != %d)",
-            len(w.body), len(v.body))
-    }
-    for i, b := range w.body {
-        if b != v.body[i] {
-            t.Errorf("vector testing: Copy() is incorrect, v2.Copy().body != v2.body (%v != %v)",
-                w.body, v.body)
-        }
-    }
-    if w.lenLast != v.lenLast {
-        t.Errorf("vector testing: Copy() is incorrect, v2.Copy().lenLast != v2.lenLast (%v != %v)",
-            w.lenLast, v.lenLast)
+    if !w.Equal(v) {
+        t.Errorf("vector testing: Copy() is incorrect, %v != %v",
+            w, v)
     }
 }
 
@@ -111,18 +73,8 @@ func TestCopyLenMore64(t *testing.T) {
         0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1,
     })
     w := v.Copy()
-    if len(w.body) != len(v.body) {
-        t.Errorf("vector testing: Copy() is incorrect, len(v3.Copy()) != v3 (%d != %d)",
-            len(w.body), len(v.body))
-    }
-    for i, b := range w.body {
-        if b != v.body[i] {
-            t.Errorf("vector testing: Copy() is incorrect, v3.Copy().body != v3.body (%v != %v)",
-                w.body, v.body)
-        }
-    }
-    if w.lenLast != v.lenLast {
-        t.Errorf("vector testing: Copy() is incorrect, v3.Copy().lenLast != v3.lenLast (%v != %v)",
-            w.lenLast, v.lenLast)
+    if !w.Equal(v) {
+        t.Errorf("vector testing: Copy() is incorrect, %v != %v",
+            w, v)
     }
 }
