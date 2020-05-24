008a4a281828ab9208b7143b5c32991242a0dd01
Ivan Chizhov <ivchizhov@gmail.com>
2020-05-25 00:37:44 +0300

    Fix bug.

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -5,7 +5,7 @@ import "github.com/gf2crypto/blincodes-go/vector"
 //Matrix represents binary Matrix
 //This type is immutable
 type Matrix struct {
-    body     []vector.Vector
+    body     [](*vector.Vector)
     nrows    int
     ncolumns int
 }
